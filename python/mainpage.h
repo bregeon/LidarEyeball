127b0906f3eeb80a6446bfa5ad7cfe119778aea3
bregeon <bregeon@681173a9-d01c-4a78-b64a-fefc38927090>
2013-11-29 12:17:45 +0000

    improve doc

--- python/mainpage.h
+++ python/mainpage.h
@@ -9,25 +9,42 @@
  *  LidarEyeball package to handle HESS Lidar runs
  *
  *  - @ref pLidarRun : pLidarRun.py contains the pLidarRun class to analyze
- Lidar data
+*     Lidar data
  *  - @ref pLidarRunPlotter : pLidarRunPlotter.py contains the pLidarRunPlotter
- calss to plot results from the Lidar analysis done with a pLidarRun object
+*     class to plot results from the Lidar analysis done with a pLidarRun object
+ *  - pRayleigh.py and pRayleighPlotter.py estimate the Rayleigh scattering
+ *    contribution
+ *  - @ref pTriggerRate : pTriggerRate.py estimates a simple trigger rate from
+*     HESS camera ROOT files
+ *  - pSashInterface.py : interface to HESS ROOT Sash data format (Sash::DataSet)
+ *  - pDataInterface.py : interface to data on disk by run number, file path or
+ *    directory
  *  - @ref tools : toolBox.py
  *
- * @section pLidarRun Data analysis
+ * @section pLidarRun Lidar Data analysis
  *
- * data analysis
+ * Lidar data analysis
  * 
  * @section pLidarRunPlotter Analysis plotter
  *
- * analysis plotter
+ * Lidar data analysis plotter
+ *
+ * @section pTriggerRate Camera Data analysis for Trigger rate
+ *
+ * Basic HESS camera data analysis to estimate a (corrected) trigger rate
  *
  * @section tools Lidar analysis tool box
  *
  * toolBox.py contains a set of functions to do stuff
  *
  * <hr>
- * @todo write documentation
- *
+ * @todo pTriggerRate.py
+ *  - be able to combine multiple runs in one plot
+ *  - fix get minimum and maximum time stamps in fillRawTriggerRate
+ * @todo pSashInterface.py
+ *  - really create a Sash::DatSet given a run number or a list of files
+ * @todo pDataInterface.py
+ *  - either give back a list of files for a run number
+ *  - or give back Sash::DataSet for a given run number
 */
 
--- python/pTriggerRate.py
+++ python/pTriggerRate.py
@@ -17,17 +17,32 @@ from pDataInterface import *
 #
 class pTriggerRate(object):
     ####################################
-    ## @brief Constructor for the interface
+    ## @brief Constructor
     #
     ## @param self
     #  the object instance
     ## @param runNumber
-    #  a runNumber that will use to 
+    #  a runNumber, e.g. 67227
+    #  
     def __init__(self, runNumber):
+        ## run number
         self.RunNumber=runNumber
+        ## "events_tree" TChain of Camera files
         self.EventsChain=None
+        ## Histogram of the events time stamp with a 1 s binning
+        self.hTrgRate=None
+        ## Average trigger rate obtained by a fit of hTrgRate with a constant
+        self.AverageTriggerRate=-1
+        ## Canvas to plot the histogram of time stamps
+        self.TrigCan=None
+        
         self.openCamFile()
     
+    ####################################
+    ## @brief Use a TChain to open HESS ROOT camera files for a given run
+    #
+    ## @param self
+    #  the object instance
     def openCamFile(self):
          # Data are in another ROOT File - fix readFile above
          #data=ROOT.Sash.DataSet("/home/bregeon/Hess/data/run067217/run_067217_Camera_001.root")
@@ -36,22 +51,37 @@ class pTriggerRate(object):
                                      (self.RunNumber, self.RunNumber)))
          
 
+    ####################################
+    ## @brief Create a histogram of time stamps with 1 s bins
+    #  and fit it with a constant
+    #
+    ## @param self
+    #  the object instance
     def fillRawTriggerRate(self):
          hName="hTrgRate_%d"%self.RunNumber
          hTitle="Trigger Rate for Tel_%d"%self.RunNumber
          self.hTrgRate=ROOT.TH1F(hName, hTitle, 2000,-100,1900)
          self.hTrgRate.GetXaxis().SetTitle("Seconds from run start")
-         self.hTrgRate.GetYaxis().SetTitle("Raw Trigger rate (Hz)")
-         tStart=self.EventsChain.GetMinimum("fTime")
+         self.hTrgRate.GetYaxis().SetTitle("Raw Trigger rate (Hz)")         
+         tStart=self.EventsChain.GetMinimum("fTime")         
          if tStart < 10:
              tStart=self.EventsChain.GetMaximum("fTime")-1800
          self.EventsChain.Project(hName,"fTime-%f"%tStart)
          fit=self.hTrgRate.Fit("pol0","QS")
          fitparams=fit.Get().GetParams()
+         self.hTrgRate.SetMaximum(250)
          self.AverageTriggerRate=fitparams[0]
          logger.info("Run %s Average Trigger Rate: %.02f"%\
                      (self.RunNumber,self.AverageTriggerRate))
     
+    ####################################
+    ## @brief Plot histogram of the trigger rate in a TPad
+    #
+    ## @param self
+    #  the object instance
+    ## @param gPad
+    #  a TCanvas TPad to plot the histogram in
+    #
     def plotRawTriggerRate(self, gPad=None):
          if gPad is None:
              self.TrigCan=ROOT.TCanvas('TrigerCan_%s'%self.RunNumber,\
@@ -65,12 +95,5 @@ class pTriggerRate(object):
 if __name__ == '__main__':
     trg=pTriggerRate(67221)
     trg.fillRawTriggerRate()
-    trg2=pTriggerRate(67227)
-    trg2.fillRawTriggerRate()
     c=ROOT.TCanvas("Trigger", "Trigger", 30,50,850,650)
-    c.Divide(1,2)
-    c.cd(1)
     trg.plotRawTriggerRate(ROOT.gPad)
-    c.cd(2)
-    trg2.plotRawTriggerRate(ROOT.gPad)
-    c.cd()
