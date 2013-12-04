afda50ad5341e104dd28c87353749c169fec36c6
bregeon <bregeon@681173a9-d01c-4a78-b64a-fefc38927090>
2013-12-04 12:07:13 +0000

    can now find data for a list of runs

--- python/pDataInterface.py
+++ python/pDataInterface.py
@@ -6,14 +6,16 @@
 import os
 import glob
 
-from __logging__        import *
+from __logging__ import *
 
+## Default data directory on mmy laptop
 HESS_DATA_DIR='/data/Hess/data/'
 
 ####################################
 ## @brief Class to interface to data input
 #
 class pDataInterface(object):
+
     ####################################
     ## @brief Constructor for the interface
     #
@@ -21,26 +23,80 @@ class pDataInterface(object):
     #  the object instance
     ## @param directory
     #  a directory with data files
-    def __init__(self, directory='', runNumber=-1):
+    def __init__(self, runsList=[], directory=HESS_DATA_DIR):
+        self.RunsList=runsList
         self.Directory=directory
-        self.RunNumber=runNumber
+        self.AllRunsDir=[]
+        self.AllRunsDict={}
+        self.FailedRunsList=[]
+        self.__hashDataDir__()
 
 
+    ####################################
+    ## @brief Hash a directory to get HESS runs
+    #   Also remove the one for which either Lidar or Camera data
+    #   are not available
+    #
+    ## @param self
+    #  the object instance
+    def __hashDataDir__(self):
+        logger.info('Hashing input directory for HESS runs:\n%s'%self.Directory)
+        self.AllRunsDir=glob.glob(HESS_DATA_DIR+'/run*')
+        for adir in self.AllRunsDir:
+            run=int(adir.split('/run0')[1])
+            self.AllRunsDict[run]={}
+            self.AllRunsDict[run]['DataDir']=adir
+        self.__checkRuns__()
+        
+    ####################################
+    ## @brief Check that required runs are available
+    #
+    ## @param self
+    #  the object instance
+    def __checkRuns__(self):
+        for run in self.RunsList:
+            runRC=self.__hashRunDir__(run)
+            if runRC==1:
+                logger.warning('Run %s is not available. Removed from dictionnary.'%run)
+                self.RunsList.remove(run)
+                self.FailedRunsList.append(run)
+            elif runRC>=1:
+                self.RunsList.remove(run)
+                self.FailedRunsList.append(run)                
+                self.AllRunsDict.pop(run)
+                logger.warning('Data not available for run %s. Removed from list and dictionnary.'%run)
+        logger.info("Failed runs list: %s"%self.FailedRunsList)        
+                
     ####################################
     ## @brief Hash a directory to find known data files
     #
     ## @param self
     #  the object instance
-    def __hashDir__(self):
-        logger.info('Hashing input directory for HESS ROOT files:\n%s'%self.Directory)
-        rootFiles=glob.glob(os.path.join(self.Directory,'*.root'))
-        txtFiles=glob.glob(os.path.join(self.Directory,'*.txt'))
-        logger.debug(rootFiles)
-        logger.debug(txtFiles)        
+    ## @param run
+    #  a run number
+    def __hashRunDir__(self, run):
+        if run not in self.AllRunsDict.keys():
+           return 1
+        logger.debug('Hashing input directory for HESS runs directory:\n%s'%self.AllRunsDict[run]['DataDir'])
+        self.AllRunsDict[run]['LidarFiles']=glob.glob(os.path.join(self.AllRunsDict[run]['DataDir'],'run_%06d_Lidar_*.root'%run))
+        self.AllRunsDict[run]['CameraFiles']=glob.glob(os.path.join(self.AllRunsDict[run]['DataDir'],'run_%06d_Camera_*.root'%run))        
+        self.AllRunsDict[run]['TxtFiles']=glob.glob(os.path.join(self.AllRunsDict[run]['DataDir'],'*.txt'))
+        self.AllRunsDict[run]['FitsFiles']=glob.glob(os.path.join(self.AllRunsDict[run]['DataDir'],'*.fits'))
+        for key in self.AllRunsDict[run].keys():
+            print self.AllRunsDict[run]['LidarFiles']
+            if key not in ['DataDir']:
+                self.AllRunsDict[run][key].sort()
+            if len(self.AllRunsDict[run]['LidarFiles'])>0 and\
+               len(self.AllRunsDict[run]['CameraFiles'])>0:
+                return 0
+            else:           
+                return 2
 
 
 if __name__ == '__main__':
-    di=pDataInterface(os.path.join(HESS_DATA_DIR,'run067217'))
-    di.__hashDir__()
-    
+    # Unit Test 
+    di=pDataInterface(runsList=[67227,67229, 34])
+    print di.AllRunsDict.keys()
+    print di.RunsList
+    print di.FailedRunsList
     
